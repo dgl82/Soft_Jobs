daf12214b9f82ebaf055ea695d480802f3e840d8
David Gili <d.gili.lopez@gmail.com>
2026-06-03 16:58:58 -0400

    login funciona y envia token de vuelta

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express(); //Asignamos la instancia de express a la constante app
 const puerto = process.env.PORT || 3000; //Definimos el puerto de escucha del servidor con la variables ocnfigurada en archivo .env o 3000 por defecto.
 const jwt = require("jsonwebtoken"); //Importamos el paquete jsonwebtoken
 const { registrarUsuario } = require("./consultas"); //Importamos las funciones del archivo consultas.js
-const { verificarCredenciales } = require("./middlewares"); //Importamos middlewares
+const { verificarRegistro, verificarLogin, verificarCredenciales } = require("./middlewares"); //Importamos middlewares
 
 //middlewares
 app.use(cors()); //Habilitamos cors
@@ -19,7 +19,7 @@ app.listen(puerto, console.log(`Servidor encendido escuchando puerto ${puerto}`)
 //Rutas de consultas
 
 //Ruta POST para registrar usuarios
-app.post("/usuarios", verificarCredenciales, async (req, res) => {
+app.post("/usuarios", async (req, res) => {
   try {
     const usuario = req.body; //Capturamos los datos enviados por el usuario
     await registrarUsuario(usuario); //Llamamos a la función enviando los datos de registro
@@ -28,4 +28,14 @@ app.post("/usuarios", verificarCredenciales, async (req, res) => {
     console.log(error); //Mostramos el error devuelto por la BD en consola
     return res.status(500).json({ message: "Internal server error" }); //Respondemos al frontend con el mensaje 500 y un mensaje en formato JSON
   }
+});
+
+//Ruta POST para login
+//Usamos el middleware para comprobar que existe el correo de la solicitud en la BD
+app.post("/login", verificarCredenciales, async (req, res) => {
+  try {
+    const { email, password } = req.body; //Extraemos el email y password del body de la solicitud
+    const token = jwt.sign({ email }, "llave_screta"); //Generamos token mediante sign y usamos el email del usuario como payload
+    res.status(200).send(token); //Enviamos el token de vuelta al frontend y código 200 (OK)
+  } catch (error) {}
 });
--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,50 @@
-//Middleware para verificar la existencia del correo en BD
+const bcrypt = require("bcryptjs"); //Importamos paquete de encriptación para las contraseñas
+const { Pool } = require("pg"); //Importamos la clase pool del paquete pg
 
-const verificarCredenciales = (req, res, next) => {
+//Creamos una instancia de la clase Pool usando un objeto de configuración con variables de entorno del archivo .env
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  allowExitOnIdle: process.env.DB_ALLOW_EXIT === "true",
+});
+
+//Middleware para verificar la existencia de credenciales en BD en login
+
+const verificarCredenciales = async (req, res, next) => {
+  try {
+    const { email, password } = req.body; //Capturamos el email y password del body
+    const values = [email]; //Guardamos el email como value para usarlo como valor parametrizado
+    const consulta = "SELECT * FROM usuarios WHERE email = $1"; //Consulta a BD por el email ingresado
+    //Extraemos el primer objeto de la respuesta y la cantidad de registros de la respuesta de la consulta
+    const {
+      rows: [usuario],
+      rowCount,
+    } = await pool.query(consulta, values);
+
+    if (!rowCount) {
+      //Comprobamos si el usuario existe. Si no existe (rowcount = 0) se detiene la función que llama el middleware con return
+      return res.status(401).json({ message: "Email o contraseña incorrecta" }); //Respondemos mensaje 401 (No autorizado) y mensaje sin dar pistas
+    }
+    //Extraemos la contraseña encriptada del objeto respondido por la BD y la renombramos a "passwordEncriptada"
+    const { password: passwordEncriptada } = usuario;
+    const passwordEsCorrecta = bcrypt.compareSync(password, passwordEncriptada); //Comparamos el password introducido por el usuario con el encriptado en BD
+
+    if (!passwordEsCorrecta) {
+      //Si el resultado de la operación anterior es "False" el middleware detiene la función que lo llamó
+      return res.status(401).json({ message: "Email o contraseña incorrecta" }); //Respondemos mensaje 401 (No autorizado) y mensaje sin dar pistas
+    }
+
+    next(); //Damos continuidad a la función que llamó el middleware
+  } catch (error) {
+    console.error("Error en el middleware de credenciales:", error); //Mostramos error en consola del servidor
+    return res.status(500).json({ message: "Error interno del servidor" }); //Interrumpimos la función que llamó el middleware y devolvemos error 500 con mensaje
+  }
+};
+
+////////////////////////////////////////////////////
+const verificarRegistro = (req, res, next) => {
   const { email, password, rol, lenguage } = req.body; //Capturamos los datos enviados por el usuario
 
   if (
@@ -19,8 +63,23 @@ const verificarCredenciales = (req, res, next) => {
     return res.status(400).json({ message: "Todos los campos son obligatorios." });
   }
 
-  next(); //La función del middleware permite que la función que la llamó continúe con su ejecución
+  next(); //Permitimos que la función que llamó éste middleware continúe con su ejecución
 };
 
-// Exportamos la función
+//Middleware para verificar la existencia de credenciales en login
+
+const verificarLogin = (req, res, next) => {
+  const { email, password } = req.body; //Capturamos los datos enviados por el usuario
+
+  //Sólo validamos existencia de email y password y que no estén vacíos en el login
+  if (!email || !password || email.trim() === "" || password.trim() === "") {
+    //Si alguna condición se cumple, se manda de vuelta error 400 (petición incorrecta) y mensaje en formato JSON
+    //Se agrega return para que detenga la función en que se usa este middleware
+    return res.status(400).json({ message: "Email y contraseña son obligatorios." });
+  }
+
+  next(); //Permitimos que la función que llamó éste middleware continúe con su ejecución
+};
+
+// Exportamos los middlewares
 module.exports = { verificarCredenciales };
